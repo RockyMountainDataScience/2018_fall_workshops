037fa01fa74773b27b94fbcfaf46412d18a440b7
Jordan Love <jordnlv@gmail.com>
2018-08-27 22:32:31 -0600

    Created folder for workshop 1 and added the basic helloworld.py script

---
+++ workshop1/helloworld.py
@@ -0,0 +1,8 @@
+# This is a simple script in python
+# By placing the # at the beginning of the line,
+# the line is not executed by the python interpreter
+
+# Modify the sentence between the quotation marks
+# to something else!
+
+print("Hello World!")
