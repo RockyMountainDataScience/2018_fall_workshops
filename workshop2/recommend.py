5a67500eabfe4bcc996619d8323e4845d9dfc7a0
Jordan Love <jordnlv@gmail.com>
2018-09-17 21:17:33 -0600

    Added template file recommend.py and test cases for recommend function as test_cases.txt

---
+++ workshop2/recommend.py
@@ -0,0 +1,13 @@
+# This file contains the a skeleton of what needs to be accomplished during the Workshop
+# Below we have a dictionary definition of a variable named 'friends' -- Think about what
+# information should go in this data structure
+
+friends = {}
+
+# Next, we have a the beginnings of a definition of a function named 'recommend'
+# Think about what variable names should be and what the function should do
+
+def recommend(variable_name):
+    return 'what should we return?'
+
+
