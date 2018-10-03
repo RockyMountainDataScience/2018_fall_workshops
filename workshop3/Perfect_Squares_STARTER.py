daa9cd0fca6834a8a905c8fb7b074e43d4ba7a4c
Catherine Potts <41930795+pottscg@users.noreply.github.com>
2018-10-03 15:28:49 -0600

    PERFECT_SQUARES_STARTER
    This is a starter file for WS 3 application

---
+++ workshop3/Perfect_Squares_STARTER.py
@@ -0,0 +1,18 @@
+# PERFECT_SQUARES
+# Author: 
+# This code will read from a file and count perfect squares
+
+#open the file enter in your filepath
+fileLink = open( FILEPATH_HERE , 'r')
+
+#Create a storage space for your count
+
+# for each line in the file, decide whether or not it is a
+# perfect square and update your counts
+for line in fileLink:
+
+
+#close the file
+fileLink.close()
+
+# show your results
